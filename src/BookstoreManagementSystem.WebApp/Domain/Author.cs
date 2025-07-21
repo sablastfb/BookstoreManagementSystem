508c0b589fef503e7a2496a62afc01e8660204ea
sablastBF <filip.barbaric99@gmail.com>
2025-07-21 02:09:55 +0200

    Added some validation to Author.cs

--- src/BookstoreManagementSystem.WebApp/Domain/Author.cs
+++ src/BookstoreManagementSystem.WebApp/Domain/Author.cs
@@ -1,8 +1,13 @@
-﻿namespace BookstoreManagementSystem.WebApp.Domain;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookstoreManagementSystem.WebApp.Domain;
 
 public class Author
 {
   public Guid Id { get; init; }
+  
+  [MinLength(1)]
+  [MaxLength(200)]
   public string? Name { get; init; } 
   public int BirthYear { get; init; } 
   public DateTime CreatedAt { get; init; }
--- src/BookstoreManagementSystem.WebApp/Features/Authors/Create.cs
+++ src/BookstoreManagementSystem.WebApp/Features/Authors/Create.cs
@@ -17,7 +17,21 @@ public class Create
   {
     public AuthorDataValidator()
     {
-      RuleFor(x => x.Name).NotNull().NotEmpty();
+      RuleFor(x => x.BirthYear)
+        .GreaterThan(0)
+        .WithMessage("Birth year must be greater than 0")
+        .LessThanOrEqualTo(DateTime.Now.Year)
+        .WithMessage("Birth year cannot be in the future")
+        .GreaterThanOrEqualTo(1000)
+        .WithMessage("Birth year must be realistic (after year 1000)");
+      
+      RuleFor(x => x.Name)
+        .NotEmpty()
+        .WithMessage("Author name is required")
+        .Length(1, 200)
+        .WithMessage("Author name must be between 1 and 200 characters")
+        .Matches(@"^[a-zA-Z\s\-\.\']+$")
+        .WithMessage("Author name can only contain letters, spaces, hyphens, periods, and apostrophes");
     }
   }
 
