a6002a739ab89c416d242cec20a9ec2c6ad68335
sablastBF <filip.barbaric99@gmail.com>
2025-07-21 03:23:10 +0200

    Added endpoint for jwt.

---
+++ src/BookstoreManagementSystem.WebApp/Features/Users/Login.cs
@@ -0,0 +1,42 @@
+﻿using FluentValidation;
+using MediatR;
+
+namespace BookstoreManagementSystem.WebApp.Features.Users;
+
+public class Login
+{
+  public record UserData(string Username, string Password);
+  public record UserEnvelope(string Token);
+  
+  public class UserDataValidator : AbstractValidator<UserData>
+  {
+    public UserDataValidator()
+    {
+      RuleFor(x => x.Username).NotNull().NotEmpty().WithMessage("Username is required.");
+      RuleFor(x => x.Password).NotNull().NotEmpty().WithMessage("Password is required.");
+    }
+  }
+
+  public record Command(UserData User) : IRequest<UserEnvelope>;
+
+  public class CommandValidator : AbstractValidator<Command>
+  {
+    public CommandValidator()
+    {
+      RuleFor(x => x.User).NotNull().SetValidator(new UserDataValidator());
+    }
+  }
+
+  public class Handler 
+    : IRequestHandler<Command, UserEnvelope>
+  {
+    public Task<UserEnvelope> Handle(
+      Command request,
+      CancellationToken cancellationToken
+    )
+    {
+      var userEnvelope = new UserEnvelope("234");
+      return Task.FromResult(userEnvelope);
+    }
+  }
+}
---
+++ src/BookstoreManagementSystem.WebApp/Features/Users/UsersController.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookstoreManagementSystem.WebApp.Features.Users;
+
+[Route("users")]
+public class UsersController(IMediator mediator): Controller
+{
+  [HttpPost("login")]
+  public Task<Login.UserEnvelope> Login([FromBody] Login.Command command,  CancellationToken cancellationToken)
+    => mediator.Send(command, cancellationToken);
+}
